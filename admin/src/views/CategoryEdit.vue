0bd162830388e3f61e198ae1129a734a2f74626d
彼时年少 <1958672699@qq.com>
2020-07-10 12:00:56 +0800

    完成分类修改和删除

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="about">
-    <h1>创建分类</h1>
-    <el-form label-width='120px' @submit.native.prevent="save">
-      <el-form-item label='名称'>
+    <h1>{{id ? '编辑' : '新建'}}分类</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type='primary' native-type='submit'>提交</el-button>
+        <el-button type="primary" native-type="submit">提交</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
 export default {
-  data(){
+  props: {
+    id: {}
+  },
+  data() {
     return {
-      model : {}
+      model: {}
+    };
+  },
+  methods: {
+    async save() {
+      let res;
+      if (this.id) {
+        res = await this.$http.put(`categories/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("categories", this.model);
+      }
+      this.model = res.data;
+      this.$router.push("/categories/list");
+      this.$message({
+        type: "success",
+        message: "保存成功"
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`categories/${this.id}`);
+      this.model = res.data;
     }
   },
-  methods:{
-    async save(){
-      const res = await this.$http.post('categories', this.model)
-      this.$router.push('/categories/list')
-      this.$message({
-        type: 'success',
-        message: '保存成功'
-      })
-    }
+  created() {
+    this.id && this.fetch();
   }
-}
+};
 </script>
